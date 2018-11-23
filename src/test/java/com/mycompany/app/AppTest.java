8fef18f35f4745b0bd8e586148e96b7dcf8005a8
dyamini <45255899+dyamini@users.noreply.github.com>
2018-11-23 16:55:30 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -35,6 +35,7 @@ public class AppTest
         App.main(null);
         try {
             assertEquals("Hello World! new change" + System.getProperty("line.separator"), outContent.toString());
+            
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
